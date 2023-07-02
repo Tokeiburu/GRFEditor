7b6bcc140d70c7c22aff6bbdce183984843814ce
Tokeiburu <sylvaingue@gmail.com>
2023-07-02 12:47:16 -0400

    Act Editor stuff.

---
+++ TokeiLibrary/WPF/HyperlinkTextBlock.xaml.cs
@@ -0,0 +1,83 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace TokeiLibrary.WPF {
+	/// <summary>
+	/// Interaction logic for LinkControl.xaml
+	/// </summary>
+	public partial class HyperlinkTextBlock : UserControl {
+		#region Delegates
+
+		public delegate void LinkControlDelegate(object sender);
+
+		#endregion
+
+		public static DependencyProperty NormalBrushProperty = DependencyProperty.Register("NormalBrush", typeof (Brush), typeof (HyperlinkTextBlock), new PropertyMetadata(new SolidColorBrush(Color.FromArgb(255, 0, 0, 254)), new PropertyChangedCallback(OnNormalBrushChanged)));
+		public static DependencyProperty MouseOverBrushProperty = DependencyProperty.Register("MouseOverBrush", typeof (Brush), typeof (HyperlinkTextBlock), new PropertyMetadata(new SolidColorBrush(Color.FromArgb(255, 0, 91, 255)), new PropertyChangedCallback(OnMouseOverBrushChanged)));
+		public static DependencyProperty HeaderProperty = DependencyProperty.Register("Header", typeof (string), typeof (HyperlinkTextBlock), new PropertyMetadata(new PropertyChangedCallback(OnHeaderChanged)));
+
+		public HyperlinkTextBlock() {
+			InitializeComponent();
+
+			_label.Foreground = NormalBrush;
+			MouseLeftButtonUp += (e, a) => OnClick();
+		}
+
+		public Brush NormalBrush {
+			get { return (Brush) GetValue(NormalBrushProperty); }
+			set { SetValue(NormalBrushProperty, value); }
+		}
+
+		public Brush MouseOverBrush {
+			get { return (Brush) GetValue(MouseOverBrushProperty); }
+			set { SetValue(MouseOverBrushProperty, value); }
+		}
+
+		public string Header {
+			get { return (string) GetValue(HeaderProperty); }
+			set { SetValue(HeaderProperty, value); }
+		}
+
+		public event LinkControlDelegate Click;
+
+		public void OnClick() {
+			LinkControlDelegate handler = Click;
+			if (handler != null) handler(this);
+		}
+
+		private static void OnNormalBrushChanged(DependencyObject d, DependencyPropertyChangedEventArgs e) {
+			HyperlinkTextBlock link = d as HyperlinkTextBlock;
+
+			if (link != null) {
+				link._label.Foreground = (Brush) e.NewValue;
+			}
+		}
+
+		private static void OnMouseOverBrushChanged(DependencyObject d, DependencyPropertyChangedEventArgs e) {
+		}
+
+		private static void OnHeaderChanged(DependencyObject d, DependencyPropertyChangedEventArgs e) {
+			HyperlinkTextBlock link = d as HyperlinkTextBlock;
+
+			if (link != null) {
+				link._label.Text = e.NewValue.ToString();
+			}
+		}
+
+		protected override void OnMouseEnter(MouseEventArgs e) {
+			Mouse.OverrideCursor = Cursors.Hand;
+			_label.Foreground = MouseOverBrush;
+			_label.SetValue(TextBlock.TextDecorationsProperty, TextDecorations.Underline);
+			base.OnMouseEnter(e);
+		}
+
+		protected override void OnMouseLeave(MouseEventArgs e) {
+			Mouse.OverrideCursor = null;
+			_label.Foreground = NormalBrush;
+			_label.SetValue(TextBlock.TextDecorationsProperty, null);
+			base.OnMouseLeave(e);
+		}
+	}
+}
---
+++ TokeiLibrary/WpfBugFix/ToggleMemoryPopup.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using TokeiLibrary.WPF.Styles;
+using TokeiLibrary.WPF.Styles.ListView;
+
+namespace TokeiLibrary.WpfBugFix {
+	/// <summary>
+	/// Interaction logic for ToggleMemoryPopup.xaml
+	/// </summary>
+	public partial class ToggleMemoryPopup : TkWindow {
+		internal readonly RangeListView _listView = new RangeListView();
+
+		public ToggleMemoryPopup() {
+			InitializeComponent();
+
+			ListViewDataTemplateHelper.GenerateListViewTemplateNew(_listView, new ListViewDataTemplateHelper.GeneralColumnInfo[] {
+			    new ListViewDataTemplateHelper.GeneralColumnInfo {Header = "Commands", DisplayExpression = "CommandDescription", FixedWidth = 230, TextAlignment = TextAlignment.Left, ToolTipBinding = "CommandDescription" }
+			}, null, new string[] { }, "generateHeader", "false");
+
+			_gridSearchContent.Children.Add(_listView);
+
+			_listView.MaxHeight = 225;
+			_listView.FocusVisualStyle = null;
+			_listView.SetValue(VirtualizingStackPanel.IsVirtualizingProperty, true);
+			_listView.BorderThickness = new Thickness(0);
+			_listView.Padding = new Thickness(0);
+			_listView.SetValue(ScrollViewer.VerticalScrollBarVisibilityProperty, ScrollBarVisibility.Auto);
+			_listView.Background = Brushes.Transparent;
+		}
+	}
+}
